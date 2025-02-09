b83c7682c062ab3d989904da0dee1d1d64b30f74
Sayabata Karki <sabyatakarki05@gmail.com>
2025-02-09 14:58:08 +0545

    cart created

--- app/src/main/java/com/example/hamro_currentnoodles/adapter/CardAdapter.kt
+++ app/src/main/java/com/example/hamro_currentnoodles/adapter/CardAdapter.kt
@@ -5,16 +5,15 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hamro_currentnoodles.databinding.CartItemBinding
 
-class CardAdapter (private  val cartItems:MutableList<String>,private val CartiIemPrice:MutableList<String>,private val CartImage:MutableList<Int>)
+class CardAdapter (private  val CartItems:MutableList<String>,private val CartiIemPrice:MutableList<String>,private val CartImage:MutableList<Int>)
     : RecyclerView.Adapter<CardAdapter.CartViewHolder>() {
-        private val itemQuantities = IntArray(cartItems.size){1}
+    private val itemQuantities = IntArray(CartItems.size) { 1 }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
-        val binding = CartItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = CartItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CartViewHolder(binding)
     }
-
 
 
     override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
@@ -22,22 +21,66 @@ class CardAdapter (private  val cartItems:MutableList<String>,private val CartiI
 
     }
 
-    override fun getItemCount(): Int  = cartItems.size
+    override fun getItemCount(): Int = CartItems.size
 
-    inner class CartViewHolder (private val binding:CartItemBinding):RecyclerView.ViewHolder(binding.root){
+    inner class CartViewHolder(private val binding: CartItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
             binding.apply {
                 val quantity = itemQuantities[position]
-                cartName.text = cartItems[position]
+                cartName.text = CartItems[position]
                 cartPrice.text = CartiIemPrice[position]
                 cartImage.setImageResource(CartImage[position])
                 cartItemQuantity.text = quantity.toString()
+
+                minusBtn.setOnClickListener {
+                    decreseQuantity(position)
+
+                }
+                addBtn.setOnClickListener {
+                    increseQuantity(position)
+
+                }
+                trashBtn.setOnClickListener {
+                    val itemPosition = adapterPosition
+                    if(itemPosition!= RecyclerView.NO_POSITION){
+                        deleteItem(itemPosition)
+                    }
+
+                }
+
+            }
+        }
+
+        private fun increseQuantity(position: Int) {
+            if (itemQuantities[position] < 10) {
+                itemQuantities[position]++
+                binding.cartItemQuantity.text = itemQuantities[position].toString()
             }
 
+        }
 
+        private fun decreseQuantity(position: Int) {
+            if (itemQuantities[position] > 0) {
+                itemQuantities[position]--
+                binding.cartItemQuantity.text = itemQuantities[position].toString()
+            }
+        }
+
+        private fun deleteItem(position: Int) {
+            CartItems.removeAt(position)
+            CartImage.removeAt(position)
+            CartiIemPrice.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, CartItems.size)
 
         }
 
     }
 }
+
+
+
+
+
 
--- app/src/main/java/com/example/hamro_currentnoodles/ui/fragment/ProductsFragment.kt
+++ app/src/main/java/com/example/hamro_currentnoodles/ui/fragment/ProductsFragment.kt
@@ -5,56 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hamro_currentnoodles.R
+import com.example.hamro_currentnoodles.adapter.CardAdapter
+import com.example.hamro_currentnoodles.databinding.FragmentProductsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProductsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProductsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding : FragmentProductsBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        binding = FragmentProductsBinding.inflate(inflater,container,false)
+
+
+
+        val cartFoodName = listOf( "ChessySpicyFlavo","3xChickenFlavor","hot Flavor","Lemon flavor","2x spicy")
+        val cartItemPrice = listOf( "NPR 55","NPR 50","NPR 50","NPR 55","NPR 50")
+        val cartImage = listOf(
+            R.drawable.pinkcurrent,
+            R.drawable.threex,
+            R.drawable.hot,
+            R.drawable.lemom,
+            R.drawable.twoxspicy,
+        )
+        val adapter = CardAdapter(ArrayList(cartFoodName),ArrayList(cartItemPrice),ArrayList(cartImage))
+        binding.ProductRecycle.layoutManager = LinearLayoutManager(requireContext())
+        binding.ProductRecycle.adapter = adapter
+        return binding.root
         return inflater.inflate(R.layout.fragment_products, container, false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+
     }
 }
+
