424cc38a6b74c2fcd5efa9f92f25a2fdb32f4b69
Sayabata Karki <sabyatakarki05@gmail.com>
2025-02-18 16:37:50 +0545

    add item activity added

---
+++ app/src/main/java/com/example/hamro_currentnoodles/ui/activity/AddItemActivity.kt
@@ -0,0 +1,29 @@
+package com.example.hamro_currentnoodles.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hamro_currentnoodles.R
+import com.example.hamro_currentnoodles.databinding.ActivityAddItemBinding
+
+class AddItemActivity : AppCompatActivity() {
+    private val binding: ActivityAddItemBinding by lazy {
+        ActivityAddItemBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
+
