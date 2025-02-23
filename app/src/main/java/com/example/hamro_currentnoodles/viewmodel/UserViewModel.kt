b3960b658de6139940bf9e182369670e4d8c1aa2
Sayabata Karki <sabyatakarki05@gmail.com>
2025-02-23 14:23:47 +0545

    backend of login

--- app/src/main/java/com/example/hamro_currentnoodles/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/hamro_currentnoodles/viewmodel/UserViewModel.kt
@@ -1,4 +1,42 @@
 package com.example.hamro_currentnoodles.viewmodel
 
-class UserViewModel {
+import com.example.hamro_currentnoodles.model.UserModel
+import com.example.hamro_currentnoodles.repository.UserRepository
+import com.google.firebase.auth.FirebaseUser
+
+class UserViewModel(var repo: UserRepository) {
+
+    fun login(
+        email: String, password: String,
+        callback: (Boolean, String) -> Unit
+    ) {
+        repo.login(email, password, callback)
+    }
+
+
+    fun signup(
+        email: String, password: String,
+        callback: (Boolean, String, String) -> Unit
+    ) {
+        repo.signup(email, password, callback)
+    }
+
+    fun addUserToDatabase(
+        userId: String, userModel: UserModel,
+        callback: (Boolean, String) -> Unit
+    ) {
+        repo.addUserToDatabase(userId, userModel, callback)
+    }
+
+    fun forgetPassword(
+        email: String,
+        callback: (Boolean, String) -> Unit
+    ) {
+        repo.forgetPassword(email, callback)
+    }
+
+    fun getCurrentUser(): FirebaseUser? {
+        return repo.getCurrentUser()
+    }
+
 }
