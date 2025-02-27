faa83a8d0eabafa1f8102bd114e41e35c75df54a
Sayabata Karki <sabyatakarki05@gmail.com>
2025-02-27 19:38:27 +0545

    binding for forgotpass

--- app/src/main/java/com/example/hamro_currentnoodles/ui/activity/UserLoginActivity.kt
+++ app/src/main/java/com/example/hamro_currentnoodles/ui/activity/UserLoginActivity.kt
@@ -55,6 +55,11 @@ class UserLoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.FPass.setOnClickListener {
+            val intent = Intent(this,ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
